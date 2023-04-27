2eb11cd970e8aae924db073b412f5080f409bf7d
LENOVO <LENOVO@DESKTOP-9ST5GBJ>
2023-04-27 22:39:09 +0530

    second commit

---
+++ MavenId2/src/main/resources/SecondMaven/SecondClass.java
@@ -0,0 +1,12 @@
+package SecondMaven;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this maven second project");
+		
+
+	}
+
+}
---
+++ MavenId2/src/test/java/MavenTest1/MavenTest2.java
@@ -0,0 +1,14 @@
+package MavenTest1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MavenTest2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
